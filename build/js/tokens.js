1e8d1a254fa5be954f8201ab48a25ec259ed3dc1
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-01-10 10:22:44 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 10 Jan 2023 13:19:38 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
